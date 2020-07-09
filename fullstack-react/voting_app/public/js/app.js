d64aef26fb92b4c075b3ea6cf0519e17fc2cad9c
Dari Alves <darieltondf@gmail.com>
2020-07-09 18:29:01 -0300

    binding custom component methods

--- fullstack-react/voting_app/public/js/app.js
+++ fullstack-react/voting_app/public/js/app.js
@@ -1,5 +1,9 @@
 // Component parent
 class ProductList extends React.Component {
+    handleProductUpVote(productId) {
+        console.log(productId + ' was upvoted.');
+    }
+
     render() {
         const products = Seed.products.sort((a, b) => (
             b.votes - a.votes
@@ -14,6 +18,7 @@ class ProductList extends React.Component {
                 votes={product.votes}
                 submitterAvatarUrl={product.submitterAvatarUrl}
                 productImageUrl={product.productImageUrl}
+                onVote={this.handleProductUpVote}
             />
         ));
         return (
@@ -26,6 +31,16 @@ class ProductList extends React.Component {
 
 // Component Product - component child
 class Product extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id)
+    }
+
     render () {
         return (
             <div className="item">
@@ -34,7 +49,7 @@ class Product extends React.Component {
                 </div>
                 <div className="middle aligned content">
                     <div className="header">
-                        <a>
+                        <a onClick={this.handleUpVote}>
                             <i className="large caret up icon" />
                         </a>
                         {this.props.votes}
